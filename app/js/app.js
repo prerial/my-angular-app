fad0bb7d5519c4d4fba4302e4406c053febd608f
Shvab Ostap <shvabuk@gmail.com>
2014-11-18 17:24:58 +0400

    abouth delete

--- app/js/app.js
+++ app/js/app.js
@@ -5,13 +5,12 @@ app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'loginCtrl'});
   $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeCtrl'});
   $routeProvider.when('/directive', {templateUrl: 'partials/directive.html', controller: 'directiveCtrl'});
-  $routeProvider.when('/abouth', {templateUrl: 'partials/abouth.html', controller: 'abouthCtrl'});
   $routeProvider.when('/form', {templateUrl: 'partials/form.html', controller: 'formCtrl'});
   $routeProvider.otherwise({redirectTo: '/home'});
 }]);
 
 app.run(['$rootScope', '$location', 'loginService', function($rootScope, $location, loginService){
-	var routespermission=['/directive', '/abouth', '/form'];  //route that require login
+	var routespermission=['/directive', '/form'];  //route that require login
 	$rootScope.$on('$routeChangeStart', function(){
 		if( routespermission.indexOf($location.path()) !=-1){
 			if(!loginService.islogged()){
