d8f9d075a45280708acba74d90dfcdd8bd238169
Shvab Ostap <shvabuk@gmail.com>
2014-11-24 18:32:00 +0400

    bug fix

--- data/user.php
+++ data/user.php
@@ -1,6 +1,6 @@
 <?php 
 	$user=json_decode(file_get_contents('php://input'));  //get user from 
-	if($user->mail=='demo' && $user->pass=='demo') 
+	if($user->name=='demo' && $user->pass=='demo') 
 		session_start();
 		$_SESSION['uid']=uniqid('ang_');
 		print $_SESSION['uid'];
